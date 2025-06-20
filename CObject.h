ed6f31704e3c9e6e1bbd368f45d0d3843b014c94
장민재 <wkdalswo6850@gmail.com>
2025-06-20 16:50:01 +0900

    Add CObject class implementation

---
+++ CObject.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+
+class CObject {
+protected:
+    int id;
+    std::wstring name; 
+    int atk;
+    int def;
+    int hp;
+
+public:
+    CObject(int id, const std::wstring& name, int atk, int def, int hp);
+
+    // Getter 함수들
+    int GetID() const;
+    const std::wstring& GetName() const; 
+    int GetAtk() const;
+    int GetDef() const;
+    int GetHP() const;
+
+   
+};
