f3f29a950ed6cfef82ced4e34b7f437f56abbe98
최태욱 <chlx357@gmail.com>
2025-06-20 17:20:24 +0900

    bugfix 컴파일 에러 임시처리

--- Source/Private/CMonster.cpp
+++ Source/Private/CMonster.cpp
@@ -1,8 +1,9 @@
 ﻿#include "CMonster.h"
 
+
 CMonster::CMonster(int id, const std::string& name, int atk, int def, int hp,
     EMonsterType type, int exp, const std::vector<int>& dropItemTableIDs)
-    : Object(id, name, atk, def, hp),
+    : //CGameObject(id, name, atk, def, hp), 컴파일 에러로 인한 임시처리.
     type(type), expReward(exp), dropItemTableIDs(dropItemTableIDs)
 {
 }
