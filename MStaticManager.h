da57e0333abd9b43ed6ec9e9777126af495b8037
장민재 <wkdalswo6850@gmail.com>
2025-06-20 11:37:52 +0900

    Monster class update

--- MStaticManager.h
+++ MStaticManager.h
@@ -1,1 +1,22 @@
-#pragma once
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class Monster{
+public:
+    enum class MonsterType { Normal, Boss };
+
+private:
+    MonsterType type;
+    int expReward;
+    std::vector<int> dropItemIds;
+
+public:
+    Monster(int id, const std::string& name, int atk, int def, int hp,
+        MonsterType type, int exp, const std::vector<int>& drops);
+
+   
+    MonsterType GetType() const;
+    int GetExpReward() const;
+    std::vector<int> GetDropItems() const;
+};
