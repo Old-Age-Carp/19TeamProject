2b062f8c61f2c31008ccc1700102414c501399cc
장민재 <wkdalswo6850@gmail.com>
2025-06-20 17:07:29 +0900

    Add CObject class header and implementation

---
+++ CObject.cpp
@@ -0,0 +1,31 @@
+﻿#include "CObject.h"
+
+CObject::CObject(int id, const std::wstring& name, int atk, int def, int hp)
+    : id(id), name(name), atk(atk), def(def), hp(hp)
+{
+}
+
+int CObject::GetID() const
+{
+    return id;
+}
+
+const std::wstring& CObject::GetName() const
+{
+    return name;
+}
+
+int CObject::GetAtk() const
+{
+    return atk;
+}
+
+int CObject::GetDef() const
+{
+    return def;
+}
+
+int CObject::GetHP() const
+{
+    return hp;
+}
