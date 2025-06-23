f205d1178a53f600b5ef85abf2339df45054d7d5
Jang Minjae <wkdalswo6850@gmail.com>
2025-06-23 12:52:42 +0900

    fix CStaticDataManager

--- Source/Public/CStaticDataManager.h
+++ Source/Public/CStaticDataManager.h
@@ -2,12 +2,22 @@
 
 #include "TSingleton.h"
 #include "define.h"
-#include <map>
 #include <memory>
+#include <unordered_map>
+#include <string>
+#include "CMonster.h"
 
 class CStaticDataManager : public TSingleton<CStaticDataManager>
 {
 	friend class TSingleton<CStaticDataManager>;
+
+	static CMonsterManager& GetInstance();
+
+	bool LoadMonsterData();
+
+	const FMonsterData* GetMonsterData(const std::wstring& name) const;
+
+	const FMonsterData* GetMonsterData(int id) const;
 
 public:
 	const FItemData* GetItemData(int id) const;
@@ -16,4 +26,8 @@ private:
 	std::map<int, std::unique_ptr<FItemData>> itemTable;
 	CStaticDataManager();
 	void loadItemData();
+	CMonsterManager() = default;
+	std::unordered_map<std::wstring, FMonsterData> monsterDataTable;
+	std::unordered_map<int, FMonsterData> monsterDataTableByID;
+
 };
