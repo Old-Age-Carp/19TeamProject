bc5205f5495150becf8e11fb702bdc09e3c0fdb4
Old-Age-Carp <junicrei@gmail.com>
2025-06-23 12:23:35 +0900

    h file fix
    CGameItem 클래스 추가
    define 구조체 이동
    TSingleton 생성
    cpp는 후에 다시 작성예정

--- Source/Public/CGameItem.h
+++ Source/Public/CGameItem.h
@@ -1,42 +1,19 @@
 ﻿#pragma once
 
-#include <string>
+#include "TSingleton.h"
+#include "define.h"
 #include <map>
+#include <memory>
 
-template <typename T>
-class Singleton {
-protected:
-	Singleton() {}
-	virtual ~Singleton() {}
-private:
-	Singleton(const Singleton&) = delete;
-	Singleton& operator = (const Singleton&) = delete;
-public:
-	static T& getInstance() {
-		static T instance;
-		return instance;
-	}
-};
-
-class StaticDataManager : public Singleton<StaticDataManager> {
-	friend class Singleton<StaticDataManager>;
+class StaticDataManager : public TSingleton<StaticDataManager>
+{
+	friend class TSingleton<StaticDataManager>;
 
 public:
-	struct ItemData {
-		int id;
-		std::wstring name;
-		int value;
-		int attackBonus;   //공격력 추가
-		int defenseBonus;  //방어력 추가
-		int healthBonus;   //체력 추가
-		int healAmount;    //체력 회복량
-		std::wstring type;
-	};
-	const ItemData* GetItemData(int id) const;
+	const FItemData* GetItemData(int id) const;
 
 private:
-	std::map<int, ItemData> itemTable;
+	std::map<int, std::unique_ptr<FItemData>> itemTable;
 	StaticDataManager();
 	void loadItemData();
 };
-
---
+++ TSingleton.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+template <typename T>
+class TSingleton
+{
+protected:
+	TSingleton() {}
+	virtual ~TSingleton() {}
+private:
+	TSingleton(const TSingleton&) = delete;
+	TSingleton& operator = (const TSingleton&) = delete;
+public:
+	static T& getinstance()
+	{
+		static T instance;
+		return instance;
+	}
+};
