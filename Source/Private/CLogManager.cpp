70ab6b79bd307930601b4f788c473672da57ad6e
최태욱 <chlx357@gmail.com>
2025-06-23 17:30:01 +0900

    fix 주석추가, ILogable 가상 소멸자 추가.

--- Source/Private/CLogManager.cpp
+++ Source/Private/CLogManager.cpp
@@ -1,9 +1,16 @@
-﻿#include "CLogManager.h"
+#include "CLogManager.h"
 
+using namespace std;
 /// <param name="log">내용</param>
 /// <returns>몇 번째 로그인지</returns>
 int CLogManager::AddLog(std::unique_ptr<ILogable> log)
 {
 	mlogs.push_back(std::move(log));
-    return 0;
+    return mlogs.size();
+}
+
+int CLogManager::AddLog(std::wstring logText)
+{
+	mlogs.push_back(make_unique<LogWString>(logText));
+	return mlogs.size();
 }
