57083b07e96a66357a69014fad93d49d6902aee2
Jang Minjae <wkdalswo6850@gmail.com>
2025-06-23 11:01:30 +0900

    fix 몬스터 나열 줄바꾸기

--- Source/Private/CMonsterManager.cpp
+++ Source/Private/CMonsterManager.cpp
@@ -10,28 +10,76 @@ bool CMonsterManager::LoadMonsterData()
     
     try
     {
-        MonsterData goblin = { L"고블린", 10, 50, EMonsterType::Normal, 20, {1001} }; // 드롭 아이템 ID 1001 (작은 물약)
+        MonsterData goblin = { L"고블린",
+            10,
+            50,
+            EMonsterType::Normal,
+            20,
+            {1001}
+        }; // 드롭 아이템 ID 1001 (작은 물약)
         monsterDataTable.insert({ goblin.name, goblin }); 
 
-        MonsterData orc = { L"오크", 25, 120, EMonsterType::Normal, 50, {2001, 1001} }; // 드롭 아이템 ID 2001 (철검), 1001 (작은 물약)
+        MonsterData orc = { L"오크",
+            25,
+            120,
+            EMonsterType::Normal,
+            50,
+            {2001, 1001}
+        }; // 드롭 아이템 ID 2001 (철검), 1001 (작은 물약)
         monsterDataTable.insert({ orc.name, orc });
 
-        MonsterData slime = { L"슬라임", 5, 30, EMonsterType::Normal, 10, {} }; // 드롭 아이템 없음
+        MonsterData slime = { L"슬라임",
+            5,
+            30,
+            EMonsterType::Normal,
+            10,
+            {}
+        }; // 드롭 아이템 없음
         monsterDataTable.insert({ slime.name, slime });
 
-        MonsterData zombie = { L"좀비", 15, 70, EMonsterType::Normal, 30, {1001, 3001} }; // 작은 물약, 낡은 도복
+        MonsterData zombie = { L"좀비",
+            15,
+            70,
+            EMonsterType::Normal,
+            30,
+            {1001, 3001}
+        }; // 작은 물약, 낡은 도복
         monsterDataTable.insert({ zombie.name, zombie });
 
-        MonsterData skeleton = { L"스켈레톤", 20, 60, EMonsterType::Normal, 25, {2001} }; // 철검
+        MonsterData skeleton = { L"스켈레톤",
+            20,
+            60,
+            EMonsterType::Normal,
+            25,
+            {2001}
+        }; // 철검
         monsterDataTable.insert({ skeleton.name, skeleton });
 
-        MonsterData golem = { L"골렘", 40, 200, EMonsterType::Normal, 80, {3002, 1002} }; // 철 갑옷, 큰 물약
+        MonsterData golem = { L"골렘",
+            40,
+            200,
+            EMonsterType::Normal,
+            80,
+            {3002, 1002}
+        }; // 철 갑옷, 큰 물약
         monsterDataTable.insert({ golem.name, golem });
 
-        MonsterData lich = { L"리치", 60, 350, EMonsterType::Boss, 300, {2002, 3002, 1002} }; // 미스릴검, 철 갑옷, 큰 물약
+        MonsterData lich = { L"리치",
+            60,
+            350,
+            EMonsterType::Boss,
+            300,
+            {2002, 3002, 1002}
+        }; // 미스릴검, 철 갑옷, 큰 물약
         monsterDataTable.insert({ lich.name, lich });
 
-        MonsterData dragon = { L"드래곤", 80, 500, EMonsterType::Boss, 500, {2003, 3003, 1002} }; // 전설의명검, 불멸의흑갑, 큰 물약
+        MonsterData dragon = { L"드래곤",
+            80,
+            500,
+            EMonsterType::Boss,
+            500,
+            {2003, 3003, 1002}
+        }; // 전설의명검, 불멸의흑갑, 큰 물약
         monsterDataTable.insert({ dragon.name, dragon });
 
       
