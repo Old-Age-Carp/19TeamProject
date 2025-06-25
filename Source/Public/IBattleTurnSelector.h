3c458e7127a6d0b5aae422efc1b984d2fec7e802
최태욱 <chlx357@gmail.com>
2025-06-25 11:32:18 +0900

    add IBattleTurnSelector

---
+++ Source/Public/IBattleTurnSelector.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "CGameObject.h"
+#include "CBattleManager.h"
+
+#include <memory>
+
+using std::unique_ptr;
+using std::shared_ptr;
+using std::vector;
+
+class IBattleTurnSelector
+{
+public:
+	virtual unique_ptr<CGameObject> GetNextTurn() = 0;
+	virtual void SetTeams(vector<const shared_ptr<CIsBattleAble>> teamList) = 0;
+};
