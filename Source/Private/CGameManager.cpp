675c20e9c6a849ccd2b29d987d3921496c4a7e9e
최태욱 <chlx357@gmail.com>
2025-06-26 20:07:44 +0900

    fix 모든 로그 출력하는 함수 출력, DropItem 버그 수정.

--- Source/Private/CGameManager.cpp
+++ Source/Private/CGameManager.cpp
@@ -491,7 +491,7 @@ vector<CItem> CGameManager::DropItem(CMonster* monster, int& outDropGolds)
 
 	const vector<int> dropTableIds = pMonsterData->dropItemTableIDs;
 
-	if (!pMonsterData->dropItemTableIDs.empty())
+	if (pMonsterData->dropItemTableIDs.empty())
 	{
 		return droppedCItems;
 	}
@@ -581,4 +581,15 @@ vector<CItem> CGameManager::DropItem(CMonster* monster, int& outDropGolds)
 	//}
 #pragma endregion
 	return droppedCItems;
+}
+
+/// <summary>
+/// 여태 있었던 모든 로그 출력
+/// </summary>
+void goLogs()
+{
+	CPrinter::PrintLine(L"모든 로그 출력");
+	CPrinter::ClearScreen();
+	CGameView::getInstance().ViewLogs(CLogManager::getInstance().GetLogs());
+	CPrinter::Pause();
 }
